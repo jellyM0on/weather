d8407c71e70eb9f096c693f085cb283bcc0f51f1
user <ylana.ong@gmail.com>
2022-11-05 06:23:25 +0800

    added initial html

--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ import './style.css'
 
 //get api
 async function getWeather(city){
-    const info = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=4b812d8f160b178f3fb173a9c1a28dff`, {mode: 'cors'})
-    const infoData = await info.json()
-    console.log(infoData);
-    console.log(infoData.weather);
-    return infoData; 
-    //
+    try{
+        const info = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=4b812d8f160b178f3fb173a9c1a28dff`, {mode: 'cors'})
+        const infoData = await info.json()
+        showWeather(infoData);
+    }
+    catch (error) {
+        alert(error);
+    }
 };
+
+function showWeather(data){
+    
+}
+
+
 
 const userForm = document.querySelector('#userInput'); 
 userForm.addEventListener('submit', (e) => {
